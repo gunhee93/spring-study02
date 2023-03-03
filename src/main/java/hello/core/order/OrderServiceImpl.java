1c51b2cda737010e8399b655ddb1a537b4a9130a
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-03-03 12:54:33 +0900

    singleton
    - 순수 자바 코드로 싱글톤 테스트
    - 스프링 컨테이너를 이용한 (싱글톤) 테스트(*항상 무상태를 유지해야한다*)
    - @Configration : CGLIB 상속 ( 실행중인 인스턴스면 재활용, 실행중이지 않으면 new 생성)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,17 +1,15 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
-public class OderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    public OderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -22,5 +20,10 @@ public class OderServiceImpl implements OrderService{
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
